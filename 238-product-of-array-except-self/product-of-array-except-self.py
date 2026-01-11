793fa7c1bbc4b1a09fd4b7f9e1128797bea088ae
William Zhou <willzhou109@gmail.com>
2026-01-11 12:40:41 -0500

    Time: 915 ms (5.03%) | Memory: 23.5 MB (9.09%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.py
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -6,16 +6,15 @@ class Solution(object):
         """
         array_left = [1] * len(nums) # array where at every index, the value is equal to the product of all values that are to the left of that index in nums
         array_right = [1] * len(nums) # array where at every index, the value is equal to the product of all values that are to the right of that index in nums
+        product = [1] * len(nums)
         for i in range(len(nums)):
             if i > 0:
                 array_left[i] = array_left[i-1] * nums[i-1]
+            product[i] *= array_left[i]
         for i in range(len(nums) - 1, -1, -1):
             if i < len(nums) - 1:
                 array_right[i] = array_right[i+1] * nums[i+1]
+            product[i] *= array_right[i]    
         print(array_left)
         print(array_right)
-        
-        product = []
-        for i in range(len(nums)):
-            product.append(array_left[i] * array_right[i])
         return product
