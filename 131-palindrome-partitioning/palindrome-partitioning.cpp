55d6421ed8887736a51deddf3a6de3f4d7b15826
William Zhou <willzhou109@gmail.com>
2026-08-29 00:08:17 -0400

    Time: 410 ms (5.08%) | Memory: 142 MB (8.23%) - LeetSync

---
+++ 131-palindrome-partitioning/palindrome-partitioning.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        if (s.size() == 1 || s.size() == 0) {
+            return true;
+        }
+        if (s[0] == s[s.size() - 1]) {
+            return isPalindrome(s.substr(1, s.size() - 2));
+        } else {
+            return false;
+        }
+    }
+
+    void backtrack(vector<string> currPartition, string remaining, vector<vector<string>>& output) {
+        if (remaining.size() == 0) {
+            //cout << "adding to output" << endl;
+            output.push_back(currPartition);
+        }
+        for (int i = 0; i < remaining.size(); ++i) {
+            string test = remaining.substr(0, i + 1);
+            std::cout << test << endl;
+            if (isPalindrome(test)) {
+                /*for (string x : currPartition) {
+                    cout << x << ", ";
+                }        
+                cout << endl;*/
+
+                currPartition.push_back(test);
+                backtrack(currPartition, remaining.substr(i + 1), output);
+                currPartition.pop_back();
+            }
+        }
+    }
+
+    vector<vector<string>> partition(string s) {
+        vector<vector<string>> output;
+        vector<string> currPartition;
+        backtrack(currPartition, s, output);
+        return output;
+    }
+};
